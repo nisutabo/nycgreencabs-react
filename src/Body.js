4d4cb2f79cb607b03bc8de5145539228c2500239
Folu Fara Obatusin <folu@apple.com>
2024-07-02 11:29:21 -0400

    CSS Styling Fixes + npm update

--- src/Body.js
+++ src/Body.js
@@ -36,111 +36,118 @@ export default class Body extends Component {
             LOWER-LEVEL ANALYSIS
           </div>
         </div>
+          <div className='row'>
+              <div className='column column-7'>
+                  <div className="pt-select pt-minimal" style={{marginTop: '0%', marginLeft: '6.6%'}}>
+                      <select onChange={this.handleSelect}>
+                          <option>Select Location</option>
+                          {locationNames}
+                      </select>
+                  </div>
+                  <br></br>
+                  <br></br>
+                  <LDChart location={this.state.locationChoice}/>
 
-        <div className ='row'>
-          <div className='column column-4'>
-          {this.state.locationChoice !== '' ?
-            <Card interactive={true}>
-              <div className='row' style={{marginLeft: '5%'}}>
-                <div className='column column-6'>
-                  <div className='sidebar-content'>
-                    {bubbleChartData[this.state.locationChoice]['totalrides'].toLocaleString()}<span className='sidebar-figure-units'> RIDES</span>
-                  </div>
-                  <div className='sidebar-label'>
-                    INBOUND FROM GREEN
-                  </div>
-                </div>
-                <div className='column column-6' style={{marginLeft: '1%'}}>
-                  <div className='sidebar-content'>
-                    {bubbleChartDataOutbound[this.state.locationChoice]['totalrides'].toLocaleString()}<span className='sidebar-figure-units'> RIDES</span>
-                  </div>
-                  <div className='sidebar-label'>
-                    OUTBOUND TO GREEN
-                  </div>
-                </div>
               </div>
-              <br></br>
-              <br></br>
-              <div className='row' style={{marginLeft: '5%'}}>
-                <div className='column column-6'>
-                  <div className='sidebar-content'>
-                    {bubbleChartDataYellow[this.state.locationChoice]['totalrides'].toLocaleString()}<span className='sidebar-figure-units'> RIDES</span>
-                  </div>
-                  <div className='sidebar-label'>
-                    INBOUND FROM YELLOW
-                  </div>
-                </div>
-                <div className='column column-6' style={{marginLeft: '1%'}}>
-                  <div className='sidebar-content'>
-                    {bubbleChartDataToYellow[this.state.locationChoice]['totalrides'].toLocaleString()}<span className='sidebar-figure-units'> RIDES</span>
-                  </div>
-                  <div className='sidebar-label'>
-                    OUTBOUND TO YELLOW
-                  </div>
-                </div>
-              </div>
-              <br></br>
-              <br></br>
-              <div className='row' style={{marginLeft: '5%'}}>
-                <div className='column column-6'>
-                  <div className='sidebar-content'>
-                    <span className='sidebar-figure-dollar'>$</span>{Math.round(bubbleChartData[this.state.locationChoice]['totalfares']).toLocaleString()}
-                  </div>
-                  <div className='sidebar-label'>
-                    TOTAL FARES INBOUND
-                  </div>
-                </div>
-                <div className='column column-6' style={{marginLeft: '1%'}}>
-                  <div className='sidebar-content'>
-                    <span className='sidebar-figure-dollar'>$</span>{Math.round(bubbleChartDataOutbound[this.state.locationChoice]['totalfares']).toLocaleString()}
-                  </div>
-                  <div className='sidebar-label'>
-                    TOTAL FARES OUTBOUND
-                  </div>
-                </div>
-              </div>
-              <br></br>
-              <br></br>
-              <div className='row' style={{marginLeft: '5%'}}>
-                <div className='column column-6'>
-                  <div className='sidebar-content'>
-                    {Math.round(bubbleChartData[this.state.locationChoice]['totalmiles']).toLocaleString()}<span className='sidebar-figure-units'> MILES</span>
-                  </div>
-                  <div className='sidebar-label'>
-                    MILES DRIVEN INBOUND
-                  </div>
-                </div>
-                <div className='column column-6' style={{marginLeft: '1%'}}>
-                  <div className='sidebar-content'>
-                    {Math.round(bubbleChartDataOutbound[this.state.locationChoice]['totalmiles']).toLocaleString()}<span className='sidebar-figure-units'> MILES</span>
-                  </div>
-                  <div className='sidebar-label'>
-                    MILES DRIVEN OUTBOUND
-                  </div>
-                </div>
-              </div>
-              <br></br>
-            </Card>
-          :
-            <Card interactive={true}>
-              <span className='sidebar-placeholder'>SELECT LOCATION TO VIEW DETAILS HERE</span>
-            </Card>
-          }
+              <div className='column column-4'>
+                  {this.state.locationChoice !== '' ?
+                      <Card interactive={true}>
+                          <div className='row' style={{marginLeft: '5%'}}>
+                              <div className='column column-6'>
+                                  <div className='sidebar-content'>
+                                      {bubbleChartData[this.state.locationChoice]['totalrides'].toLocaleString()}<span
+                                      className='sidebar-figure-units'> RIDES</span>
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      INBOUND FROM GREEN
+                                  </div>
+                              </div>
+                              <div className='column column-6' style={{marginLeft: '1%'}}>
+                                  <div className='sidebar-content'>
+                                      {bubbleChartDataOutbound[this.state.locationChoice]['totalrides'].toLocaleString()}<span
+                                      className='sidebar-figure-units'> RIDES</span>
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      OUTBOUND TO GREEN
+                                  </div>
+                              </div>
+                          </div>
+                          <br></br>
+                          <br></br>
+                          <div className='row' style={{marginLeft: '5%'}}>
+                              <div className='column column-6'>
+                                  <div className='sidebar-content'>
+                                      {bubbleChartDataYellow[this.state.locationChoice]['totalrides'].toLocaleString()}<span
+                                      className='sidebar-figure-units'> RIDES</span>
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      INBOUND FROM YELLOW
+                                  </div>
+                              </div>
+                              <div className='column column-6' style={{marginLeft: '1%'}}>
+                                  <div className='sidebar-content'>
+                                      {bubbleChartDataToYellow[this.state.locationChoice]['totalrides'].toLocaleString()}<span
+                                      className='sidebar-figure-units'> RIDES</span>
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      OUTBOUND TO YELLOW
+                                  </div>
+                              </div>
+                          </div>
+                          <br></br>
+                          <br></br>
+                          <div className='row' style={{marginLeft: '5%'}}>
+                              <div className='column column-6'>
+                                  <div className='sidebar-content'>
+                                      <span
+                                          className='sidebar-figure-dollar'>$</span>{Math.round(bubbleChartData[this.state.locationChoice]['totalfares']).toLocaleString()}
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      TOTAL FARES INBOUND
+                                  </div>
+                              </div>
+                              <div className='column column-6' style={{marginLeft: '1%'}}>
+                                  <div className='sidebar-content'>
+                                      <span
+                                          className='sidebar-figure-dollar'>$</span>{Math.round(bubbleChartDataOutbound[this.state.locationChoice]['totalfares']).toLocaleString()}
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      TOTAL FARES OUTBOUND
+                                  </div>
+                              </div>
+                          </div>
+                          <br></br>
+                          <br></br>
+                          <div className='row' style={{marginLeft: '5%'}}>
+                              <div className='column column-6'>
+                                  <div className='sidebar-content'>
+                                      {Math.round(bubbleChartData[this.state.locationChoice]['totalmiles']).toLocaleString()}<span
+                                      className='sidebar-figure-units'> MILES</span>
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      MILES DRIVEN INBOUND
+                                  </div>
+                              </div>
+                              <div className='column column-6' style={{marginLeft: '1%'}}>
+                                  <div className='sidebar-content'>
+                                      {Math.round(bubbleChartDataOutbound[this.state.locationChoice]['totalmiles']).toLocaleString()}<span
+                                      className='sidebar-figure-units'> MILES</span>
+                                  </div>
+                                  <div className='sidebar-label'>
+                                      MILES DRIVEN OUTBOUND
+                                  </div>
+                              </div>
+                          </div>
+                          <br></br>
+                      </Card>
+                      :
+                      <Card interactive={true}>
+                          <span className='sidebar-placeholder'>SELECT LOCATION TO VIEW DETAILS HERE</span>
+                      </Card>
+                  }
 
+              </div>
           </div>
-          <div className='column column-7'>
-              <div className="pt-select pt-minimal" style={{ marginTop: '0%', marginLeft: '6.6%'}}>
-                <select onChange={this.handleSelect}>
-                  <option >Select Location</option>
-                    {locationNames}
-                </select>
-              </div>
-              <br></br>
-              <br></br>
-              <LDChart location={this.state.locationChoice}/>
-
-          </div>
-        </div>
       </div>
     )
   }
